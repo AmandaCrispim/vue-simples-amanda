743c3c5d81f7b9be80532d741e45786e2c057f47
AmandaCrispim <amandacrispim@gmail.com>
2022-01-31 15:55:36 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>{{ titulo }}</h2>
+    <div class="input-group">
+     <input @keyup.enter="adicionarTarefa" v-model="novaTarefa" type="text"> 
+     <span class="input-group-btn">
+        <button @click="adicionarTarefa" class="btn btn-primary">Adicionar</button>
+       </span>
+    </div>
+    <footer>
+      <em> Altere aqui o título da sua lista de tarefas</em>
+      <input v-model="titulo" type="text"> 
+      <listartarefa :tarefas="tarefas"/>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import listartarefa from "@/components/listartarefa/"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    listartarefa
+    
+  },
+  data () {
+    return {
+        titulo: "Minha Lista de Tarefas ",
+        novaTarefa: '',
+        tarefas: [
+          { titulo: "Estudar", checked: false},
+          { titulo: "Programar", checked: true},
+        ]
+    };
+  },
+    methods: {
+      adicionarTarefa() {
+        this.tarefas.push({
+          titulo: this.novaTarefa,
+          checked: false
+        })
+        this.novaTarefa="";
+      }
+    }
+  
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  width: 40%;
+  margin: 20px auto 0px auto;
 }
+
+  ul li {
+    list-style: none;
+  }
+
+  .removed {
+    color:pink;
+  }
+
+  .removed label {
+    text-decoration: line-through;
+  }
 </style>
